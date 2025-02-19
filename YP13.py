492e9637f813505f56b47015a9f216bc04f43e37
Grifon5342 <Grifon5342@mail.ru>
2025-02-19 11:17:14 +0300

    8. Использование функций - COUNT, SUM, AVG, MIN, MAX

--- YP13.py
+++ YP13.py
@@ -15,31 +15,31 @@ age INTEGER
 """)
 
 #Создание индекса для столбца "email"
-##cursor.execute("CREATE INDEX idx_email ON Users (email)")
-##
-##
-###Дабовление нового пользователя
-##cursor.execute("INSERT INTO Users (username, email, age) VALUES (?, ?, ?)", ("newuser", "newuser@example.com", 28))
-##
-###Обновление записей
-##cursor.execute("UPDATE Users SET age = ? WHERE username = ?", (29, "newuser"))
-##
-###Удвление записей
-##cursor.execute("DELETE FROM Users WHERE username = ?", ("newuser",))
-##
-##Выполнение запросов
-##cursor.execute("SELECT * FROM Users")
-##users = cursor.fetchall()
-##Выводим результат
-##for user in users:
-##    print(user)
-##
-##Выбираем имена и возраст пользователей старше 25 лет
-##cursor.execute("SELECT username, age FROM Users WHERE age > ?", (25,))
-##results = cursor.fetchall()
-##
-##for row in results:
-##    print(row)
+#cursor.execute("CREATE INDEX idx_email ON Users (email)")
+
+
+#Дабовление нового пользователя
+cursor.execute("INSERT INTO Users (username, email, age) VALUES (?, ?, ?)", ("newuser", "newuser@example.com", 28))
+
+#Обновление записей
+cursor.execute("UPDATE Users SET age = ? WHERE username = ?", (29, "newuser"))
+
+#Удвление записей
+cursor.execute("DELETE FROM Users WHERE username = ?", ("newuser",))
+
+#Выполнение запросов
+cursor.execute("SELECT * FROM Users")
+users = cursor.fetchall()
+#Выводим результат
+for user in users:
+    print(user)
+
+#Выбираем имена и возраст пользователей старше 25 лет
+cursor.execute("SELECT username, age FROM Users WHERE age > ?", (25,))
+results = cursor.fetchall()
+
+for row in results:
+    print(row)
 
 #Выбираем и сортируем пользователей
 cursor.execute("""
@@ -53,6 +53,37 @@ results = cursor.fetchall()
 
 for row in results:
     print(row)
+
+#Подсчет общего числа пользователей
+cursor.execute("SELECT COUNT(*) FROM Users")
+total_users = cursor.fetchone()[0]
+
+print("Общее количество пользователей:", total_users)
+
+#Вычисление суммы возвратов пользователей
+cursor.execute("SELECT SUM(age) FROM Users")
+total_age = cursor.fetchone()[0]
+
+print("Общая сумма возрастов пользователей:", total_age)
+
+#Вычисление среднего возраста пользователей
+cursor.execute("SELECT AVG(age) FROM Users")
+total_avg = cursor.fetchone()[0]
+
+print("Средний возраст пользователей:", total_avg)
+
+#Нахождение минимального возраста
+cursor.execute("SELECT MIN(age) FROM Users")
+min_age = cursor.fetchone()[0]
+
+print("Минимальный возраст среди пользователей:", min_age)
+
+#Нахождение максимального возраста
+cursor.execute("SELECT MAX(age) FROM Users")
+max_age = cursor.fetchone()[0]
+
+print("Максимальный возраст среди пользователей:", max_age)
+
 
 #Сохранение и закрываем соединение
 connection.commit()
