75bc1e71963cf7e701b4f70ad57d3efee9783b6d
Grifon5342 <Grifon5342@mail.ru>
2025-02-19 12:32:21 +0300

    11. Использование методов fetchone(), fetchmany(), fetchall()

--- YP13.py
+++ YP13.py
@@ -101,7 +101,20 @@ users = cursor.fetchall()
 for user in users:
     print(user)
 
+#Выбор первого пользователя
+cursor.execute("SELECT * FROM Users")
+first_user = cursor.fetchone()
+print(first_user)
 
+#Выбор первых 5 пользователей
+cursor.execute("SELECT * FROM Users")
+first_five_users = cursor.fetchmany(5)
+print(first_five_users)
+
+#Выбор всех пользователей 
+cursor.execute("SELECT * FROM Users")
+all_users = cursor.fetchall()
+print(all_users)
 
 #Сохранение и закрываем соединение
 connection.commit()
