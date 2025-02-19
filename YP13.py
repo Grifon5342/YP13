f625cbde7cba2476cdeb194061cf216a3ffcf720
Grifon5342 <Grifon5342@mail.ru>
2025-02-19 11:00:02 +0300

    7. Комбинирование операторов

--- YP13.py
+++ YP13.py
@@ -15,32 +15,44 @@ age INTEGER
 """)
 
 #Создание индекса для столбца "email"
-#cursor.execute("CREATE INDEX idx_email ON Users (email)")
+##cursor.execute("CREATE INDEX idx_email ON Users (email)")
+##
+##
+###Дабовление нового пользователя
+##cursor.execute("INSERT INTO Users (username, email, age) VALUES (?, ?, ?)", ("newuser", "newuser@example.com", 28))
+##
+###Обновление записей
+##cursor.execute("UPDATE Users SET age = ? WHERE username = ?", (29, "newuser"))
+##
+###Удвление записей
+##cursor.execute("DELETE FROM Users WHERE username = ?", ("newuser",))
+##
+##Выполнение запросов
+##cursor.execute("SELECT * FROM Users")
+##users = cursor.fetchall()
+##Выводим результат
+##for user in users:
+##    print(user)
+##
+##Выбираем имена и возраст пользователей старше 25 лет
+##cursor.execute("SELECT username, age FROM Users WHERE age > ?", (25,))
+##results = cursor.fetchall()
+##
+##for row in results:
+##    print(row)
 
+#Выбираем и сортируем пользователей
+cursor.execute("""
+SELECT username, age, AVG(age)
+FROM Users
+GROUP BY age
+HAVING AVG(age) > ?
+ORDER BY age DESC
+""", (30,))
+results = cursor.fetchall()
 
-#Дабовление нового пользователя
-cursor.execute("INSERT INTO Users (username, email, age) VALUES (?, ?, ?)", ("newuser", "newuser@example.com", 28))
-
-#Обновление записей
-cursor.execute("UPDATE Users SET age = ? WHERE username = ?", (29, "newuser"))
-
-#Удвление записей
-cursor.execute("DELETE FROM Users WHERE username = ?", ("newuser",))
-
-#Выполнение запросов
-#cursor.execute("SELECT * FROM Users")
-#users = cursor.fetchall()
-#Выводим результат
-#for user in users:
-#    print(user)
-
-#Выбираем имена и возраст пользователей старше 25 лет
-#cursor.execute("SELECT username, age FROM Users WHERE age > ?", (25,))
-#results = cursor.fetchall()
-
-#for row in results:
-#    print(row)
-
+for row in results:
+    print(row)
 
 #Сохранение и закрываем соединение
 connection.commit()
