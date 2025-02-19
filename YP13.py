04f9059a8effcbd8fb9159adc9e8d495d5d00bbe
Grifon5342 <Grifon5342@mail.ru>
2025-02-19 09:48:45 +0300

    2. Создание и управление таблицами

--- YP13.py
+++ YP13.py
@@ -2,4 +2,18 @@ import sqlite3
 
 #Подключение к бд
 connection = sqlite3.connect("my_database.db")
+cursor = connection.cursor()
+
+#Создание таблицы Users
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Users (
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER
+)
+""")
+#Сохранение и закрываем соединение
+connection.commit()
 connection.close()
+print(1)
